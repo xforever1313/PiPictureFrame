7c0bceb0c8a7879477733e6015418a4095e836dd
Seth Hendrick <seth@shendrick.net>
2016-12-11 17:08:31 -0500

    Added Chart.js to credits.txt.

--- PiPictureFrame/License.cs
+++ PiPictureFrame/License.cs
@@ -4,12 +4,6 @@
 //    (See accompanying file ../../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace PiPictureFrame.Core
 {
     public static class License
@@ -41,6 +35,22 @@ DEALINGS IN THE SOFTWARE.";
 
         public const string Credits =
 @"The Raspbery Pi Frame was made possible by several open source projects.  Below is their information.
+
+------------ Chart.js ------------
+
+For space remaining charts on the web interface.
+
+http://www.chartjs.org/
+
+The MIT License (MIT)
+Copyright (c) 2013-2016 Nick Downie
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 ------------ Pure.css ------------
 
