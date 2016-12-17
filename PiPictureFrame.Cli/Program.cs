2723fd15b732b2a131d6be787e1918fa34ba59e0
Seth Hendrick <seth@shendrick.net>
2016-12-16 22:35:43 -0500

    Logging enabled when --enable-logging is passed in as an argument.

--- PiPictureFrame.Cli/Program.cs
+++ PiPictureFrame.Cli/Program.cs
@@ -5,7 +5,10 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 using System;
+using System.IO;
+using SethCS.Extensions;
 using PiPictureFrame.Core;
+using System.Collections.Generic;
 
 namespace PiPictureFrame.Cli
 {
@@ -13,23 +16,69 @@ namespace PiPictureFrame.Cli
     {
         static int Main( string[] args )
         {
+            List<string> argList = new List<string>( args );
+
+            bool enableLogging = false;
             if( args.Length >= 1 )
             {
-                if( ( args[0] == "--help" ) || ( args[0] == "/?" ) )
+                if( ( argList.Contains( "--help" ) || argList.Contains( "/?" ) ) )
                 {
                     PrintHelp();
                     return 0;
                 }
+                else if( argList.Contains( "--enable-logging" ) )
+                {
+                    enableLogging = true;
+                }
             }
 
-            Action<string> loggingAction = ( s => Console.WriteLine( s ) );
+            if( enableLogging )
+            {
+                using(
+                    FileStream outFile = new FileStream(
+                        "PiFrame_" + DateTime.Now.ToFileNameString() + ".log",
+                        FileMode.Create,
+                        FileAccess.Write
+                    )
+                )
+                {
+                    using( StreamWriter writer = new StreamWriter( outFile ) )
+                    {
+                        Action<string> loggingAction =
+                            delegate ( string s )
+                            {
+                                writer.WriteLine( DateTime.Now.ToTimeStampString() + "  " + s );
+                                writer.Flush();
+                                Console.WriteLine( s );
+                                Console.Out.Flush();
+                            };
+
+                        RunFrame( loggingAction );
+                    }
+                }
+            }
+            else
+            {
+                Action<string> loggingAction =
+                    delegate ( string s )
+                    {
+                        Console.WriteLine( s );
+                        Console.Out.Flush();
+                    };
+
+                RunFrame( loggingAction );
+            }
+
+            return 0;
+        }
+
+        private static void RunFrame( Action<string> loggingAction )
+        {
             using( PictureFrame frame = new PictureFrame() )
             {
                 frame.Init( loggingAction );
                 frame.Run();
             }
-
-            return 0;
         }
 
         private static void PrintHelp()
